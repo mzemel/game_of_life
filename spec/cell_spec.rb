b64cbbefe290a68c1ef91236b0dcc34c76f6ab94
Peter Kagey <pkagey@sweetspotdiabetes.com>
2016-09-09 17:14:32 -0700

    Fix tests, prove to Michael that I'm better at Ruby.

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -1,11 +1,5 @@
 describe "Cell" do
-  let(:cell) { Cell.new }
   it "has a state" do
-    expect(['alive', 'dead']).to include(cell.state)
-  end
-
-  it "can have the state set" do
-    _cell = Cell.new('alive')
-    expect(cell.state).to eq('alive')
+    expect([Cell.dead, Cell.alive]).to include(Cell.random)
   end
 end
